19813f01674ba34134bcff61a34b8b0025c69e45
Gaetan Rivet <grive@u256.net>
2020-05-26 14:14:42 +0200

    flow: add offload flag
    This flag marks flows that were successfully offloaded.
    Add the flag to the flow util dump.
    
    Signed-off-by: Gaetan Rivet <grive@u256.net>
    Change-Id: I3a7e144a2f9db9efb05fec61a8666d6ff3631b27

--- utils/flow.c
+++ utils/flow.c
@@ -434,7 +434,7 @@ flow_dump_legend(void)
     printf("L=Link Local Port)\n");
 
     printf(" Other:K(nh)=Key_Nexthop, S(nh)=RPF_Nexthop\n");
-    printf(" Flags:E=Evicted, Ec=Evict Candidate, N=New Flow, M=Modified Dm=Delete Marked\n");
+    printf(" Flags:E=Evicted, Ec=Evict Candidate, N=New Flow, M=Modified Dm=Delete Marked, Of=Offloaded\n");
     printf("TCP(r=reverse):S=SYN, F=FIN, R=RST, C=HalfClose, E=Established, D=Dead\n");
     printf("\n");
 
@@ -1437,6 +1437,8 @@ flow_dump_table(struct flow_table *ft)
                 printed += printf("M");
             if (fe->fe_flags & VR_FLOW_FLAG_DELETE_MARKED)
                 printed += printf("Dm");
+            if (fe->fe_flags & VR_FLOW_FLAG_OFFLOADED)
+                printed += printf("Of");
 
             printed += printf(", ");
             if (fe->fe_key.flow4_proto == VR_IP_PROTO_TCP) {
